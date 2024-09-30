71f377f62b90456d7a4deddf193c5009785463c6
Melih Gündüz <melih.gunduz@hotmail.com>
2024-09-30 10:01:57 +0300

    Project created

--- src/stores/example-store.ts
+++ src/stores/example-store.ts
@@ -1,15 +1,8 @@
 import { defineStore } from 'pinia';
 
-export const useCounterStore = defineStore('counter', {
-  state: () => ({
-    counter: 0,
-  }),
-  getters: {
-    doubleCount: (state) => state.counter * 2,
-  },
-  actions: {
-    increment() {
-      this.counter++;
-    },
-  },
+export const useCounterStore = defineStore('counter', () => {
+  //ref
+  //getter
+  //function
+  return {};
 });
