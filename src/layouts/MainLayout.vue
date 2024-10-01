6a6e852f28640cc451ab80ff3d93133d3a8511c6
gorkemsandikci <gorkemsandikci@gmail.com>
2024-10-01 23:26:34 +0300

    header updates & logos uploaded

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-
 import ConnectSolanaWallets from 'components/ConnectSolanaWallets.vue';
 import { useQuasar } from 'quasar';
+import { computed } from 'vue';
 
 const $q = useQuasar();
 
+const logoSrc = computed(() => $q.dark.isActive ? '/assets/images/logos/solpaw-np-light-150.png' : '/assets/images/logos/solpaw-np-dark-150.png');
 
 defineOptions({
   name: 'MainLayout',
@@ -19,9 +20,11 @@ defineOptions({
       <q-toolbar>
 
         <q-toolbar-title>
-          SolPaw Nature Patrons
-          <q-btn v-if="$route.name !== 'Home'" :ripple="false" :to="{name: 'Home'}" flat icon="home" />
+          <router-link :to="{ name: 'Home' }">
+            <img :src="logoSrc" alt="Solana Paw Nature Patron Logo" height="60" class="q-mt-md" />
+          </router-link>
         </q-toolbar-title>
+        <q-btn v-if="$route.name !== 'Home'" :ripple="false" :to="{name: 'Home'}" flat icon="home" />
 
         <q-btn :icon="$q.dark.isActive ? 'fa-solid fa-sun' : 'fa-solid fa-moon'" flat push round
                @click="$q.dark.toggle" />
