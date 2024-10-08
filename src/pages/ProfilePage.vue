6f547624641e488d0deed04947ea5c30924ab793
Melih Gündüz <melih.gunduz@hotmail.com>
2024-10-08 18:13:07 +0300

    After payment, user got his NFT

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -10,7 +10,6 @@ const { publicKey } = useWallet();
 
 // Add your public key to be admin and create collections
 const adminWallets = ['2JzZBbMECNE2wuEjph1QjqoaCGJm6gJWYrCEpVSKBAkm', 'D4fQPLmmSKESQPB9LjUru2S4QQccUHYdN5G5bGWTh2Uy'];
-// const isAdmin = computed(() => publicKey.value?.toBase58() === new PublicKey('D4fQPLmmSKESQPB9LjUru2S4QQccUHYdN5G5bGWTh2Uy').toBase58());
 
 // Convert adminWallets to PublicKey instances for comparison
 const adminPublicKeys = adminWallets.map(wallet => new PublicKey(wallet));
@@ -22,16 +21,6 @@ const isAdmin = computed(() => {
   );
 });
 
-type CollectionType = {
-  collectionAddress: string,
-  collectionName: string,
-  creatorName: string,
-  owner: string
-}
-
-const nftCollections = ref<CollectionType[]>([]);
-const options = ref<CollectionType[]>([]);
-const selectedCollection = ref<CollectionType | null>(null);
 
 const collectionInfo = ref({
   title: '',
@@ -42,11 +31,6 @@ const collectionInfo = ref({
   endDate: '',
 });
 
-const tokenInfo = ref({
-  title: '',
-  description: '',
-});
-
 const collectionErrors = ref({
   title: '',
   description: '',
@@ -55,11 +39,6 @@ const collectionErrors = ref({
   endDate: '',
 });
 
-const tokenErrors = ref({
-  title: '',
-  description: '',
-  collectionName: '',
-});
 
 const validateCollection = () => {
   collectionErrors.value = {
@@ -71,16 +50,6 @@ const validateCollection = () => {
   };
 
   return !Object.values(collectionErrors.value).some(error => error);
-};
-
-const validateToken = () => {
-  tokenErrors.value = {
-    title: tokenInfo.value.title ? '' : 'Name is required',
-    description: tokenInfo.value.description ? '' : 'Description is required',
-    collectionName: selectedCollection.value?.collectionName ? '' : 'Collection name is required',
-  };
-
-  return !Object.values(tokenErrors.value).some(error => error);
 };
 
 
@@ -100,57 +69,6 @@ async function createCollection() {
       timeout: 2000,
     });
   }
-}
-
-function getNftCollections() {
-  axios.get('http://localhost:5001/api/nftCollections/collections')
-    .then((val) => nftCollections.value = val.data).catch((err) => console.log(err));
-}
-
-
-function collectionFilterFn(val: string, update: (fn: () => void) => void) {
-  if (val === '') {
-    update(() => {
-      options.value = nftCollections.value;
-    });
-    return;
-  }
-
-  update(() => {
-    const needle = val.toLowerCase();
-    options.value = nftCollections.value.filter(v =>
-      v.collectionName.toLowerCase().includes(needle),
-    );
-  });
-}
-
-function collectionsFocusFn() {
-  if (nftCollections.value.length > 0) {
-    return;
-  } else {
-    getNftCollections();
-  }
-}
-
-function createTokenInCollection() {
-  console.log(selectedCollection.value);
-  if (validateToken()) {
-    axios.post('http://localhost:5001/api/tokens/create', {
-      collectionNftAddress: selectedCollection.value?.collectionAddress,
-      tokenName: tokenInfo.value.title,
-      tokenSymbol: 'TST',
-      tokenDesc: 'lorem',
-      customerPubKey: publicKey.value,
-    }).then((val) => console.log(val)).catch((err) => console.log(err));
-  } else {
-    $q.notify({
-      message: 'Fill empty token inputs',
-      color: 'red',
-      position: 'top',
-      timeout: 2000,
-    });
-  }
-
 }
 
 
@@ -198,49 +116,6 @@ function createTokenInCollection() {
           <q-card-actions align="center" class="q-pa-md">
             <q-btn class="full-width" color="green" icon="create" label="Create collection" no-caps
                    @click="createCollection" />
-          </q-card-actions>
-        </q-card>
-      </div>
-      <div :class="[$q.screen.lt.sm ? 'q-mx-md' : '']">
-        <q-card class="column items-center" style="max-width: 600px">
-          <q-card-section>
-            <div class="text-h5">Create new token</div>
-          </q-card-section>
-          <q-card-section horizontal>
-            <q-card-section class="q-py-none" style="max-width: 300px">
-              <q-input v-model="tokenInfo.title" :rules="[val => !!val || 'Token name is required']"
-                       class="q-mb-sm" dense hide-bottom-space
-                       label="Name" outlined type="text" />
-              <q-input v-model="tokenInfo.description" :rules="[val => !!val || 'Token description is required']"
-                       class="q-mb-sm" dense hide-bottom-space label="Description"
-                       outlined
-                       type="text" />
-            </q-card-section>
-            <q-card-section class="q-py-none" style="max-width: 300px">
-              <q-select
-                v-model="selectedCollection"
-                :options="options"
-                :rules="[val => !!val || 'Collection is required']"
-                clearable
-                input-debounce="0"
-                label="Select NFT Collection"
-                map-options
-                option-label="collectionName"
-                option-value="collectionAddress"
-                use-input
-                @filter="collectionFilterFn"
-                @focus="collectionsFocusFn"
-              >
-                <template v-slot:prepend>
-                  <q-icon name="search" />
-                </template>
-              </q-select>
-            </q-card-section>
-          </q-card-section>
-          <q-card-actions align="center" class="q-pa-md">
-            <q-btn class="full-width" color="green" icon="create" label="Create token" no-caps
-                   @click="createTokenInCollection"
-            />
           </q-card-actions>
         </q-card>
       </div>
